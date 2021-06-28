e414fdb1a421e23aed3c36a2d61bf4f9444b1401
Sam Shaaban <sam@nurelm.com>
2021-06-28 06:37:18 -0400

    Initial Commit

---
+++ AudioEngineExample/AudioEngineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioEngineExampleApp.swift
+//  AudioEngineExample
+//
+//  Created by Sami Shaaban on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioEngineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
